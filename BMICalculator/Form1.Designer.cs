328b1202bc2ca22dab33ee085e1d879e6970007e
Albert <Albert@MINIHNACIK>
2024-12-06 16:11:46 +0100

    Add project files.

---
+++ BMICalculator/Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace BMICalculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.weightTextBox = new System.Windows.Forms.TextBox();
+            this.heightTextBox = new System.Windows.Forms.TextBox();
+            this.calculateBMIButton = new System.Windows.Forms.Button();
+            this.resultLabel = new System.Windows.Forms.Label();
+            this.weightLabel = new System.Windows.Forms.Label();
+            this.heightLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // weightTextBox
+            // 
+            this.weightTextBox.Location = new System.Drawing.Point(129, 12);
+            this.weightTextBox.Name = "weightTextBox";
+            this.weightTextBox.Size = new System.Drawing.Size(100, 20);
+            this.weightTextBox.TabIndex = 0;
+            // 
+            // heightTextBox
+            // 
+            this.heightTextBox.Location = new System.Drawing.Point(129, 39);
+            this.heightTextBox.Name = "heightTextBox";
+            this.heightTextBox.Size = new System.Drawing.Size(100, 20);
+            this.heightTextBox.TabIndex = 1;
+            // 
+            // calculateBMIButton
+            // 
+            this.calculateBMIButton.Location = new System.Drawing.Point(13, 67);
+            this.calculateBMIButton.Name = "calculateBMIButton";
+            this.calculateBMIButton.Size = new System.Drawing.Size(110, 23);
+            this.calculateBMIButton.TabIndex = 2;
+            this.calculateBMIButton.Text = "CALCULATE BMI";
+            this.calculateBMIButton.UseVisualStyleBackColor = true;
+            this.calculateBMIButton.Click += new System.EventHandler(this.calculateBMIButton_Click);
+            // 
+            // resultLabel
+            // 
+            this.resultLabel.AutoSize = true;
+            this.resultLabel.Location = new System.Drawing.Point(13, 97);
+            this.resultLabel.Name = "resultLabel";
+            this.resultLabel.Size = new System.Drawing.Size(0, 13);
+            this.resultLabel.TabIndex = 3;
+            // 
+            // weightLabel
+            // 
+            this.weightLabel.AutoSize = true;
+            this.weightLabel.Location = new System.Drawing.Point(10, 15);
+            this.weightLabel.Name = "weightLabel";
+            this.weightLabel.Size = new System.Drawing.Size(113, 13);
+            this.weightLabel.TabIndex = 4;
+            this.weightLabel.Text = "Enter your weight (kg):";
+            // 
+            // heightLabel
+            // 
+            this.heightLabel.AutoSize = true;
+            this.heightLabel.Location = new System.Drawing.Point(10, 42);
+            this.heightLabel.Name = "heightLabel";
+            this.heightLabel.Size = new System.Drawing.Size(107, 13);
+            this.heightLabel.TabIndex = 5;
+            this.heightLabel.Text = "Enter your height (m):";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(243, 287);
+            this.Controls.Add(this.heightLabel);
+            this.Controls.Add(this.weightLabel);
+            this.Controls.Add(this.resultLabel);
+            this.Controls.Add(this.calculateBMIButton);
+            this.Controls.Add(this.heightTextBox);
+            this.Controls.Add(this.weightTextBox);
+            this.Name = "Form1";
+            this.Text = "Calculator BMI";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox weightTextBox;
+        private System.Windows.Forms.TextBox heightTextBox;
+        private System.Windows.Forms.Button calculateBMIButton;
+        private System.Windows.Forms.Label resultLabel;
+        private System.Windows.Forms.Label weightLabel;
+        private System.Windows.Forms.Label heightLabel;
+    }
+}
+
---
+++ BMICalculator/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BMICalculator
+{
+    public partial class Form1 : Form
+    {
+        // Enum for BMI categories
+        public enum BMICategory
+        {
+            Underweight,    // BMI < 18.5
+            NormalWeight,   // 18.5 <= BMI <= 24.9
+            OverWeight,     // 25 <= BMI <= 29.9
+            Obesity,        // 30 <= BMI <= 34.9
+            SevereObesity   // BMI >= 35
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // Button click event for calculating BMI
+        private void calculateBMIButton_Click(object sender, EventArgs e)
+        {
+            // Get weight and height values from user input
+            double weight, height;
+
+            // Validate if the input values are valid numbers
+            bool isWeightValid = double.TryParse(weightTextBox.Text, out weight);
+            bool isHeightValid = double.TryParse(heightTextBox.Text, out height);
+
+            // Check if the weight and height are valid positive numbers
+            if (isWeightValid && isHeightValid && weight > 0 && height > 0)
+            {
+                // Calculate BMI: BMI = weight / (height^2)
+                double bmi = weight / (height * height);
+
+                // Get the BMI category
+                BMICategory category = GetBMICategory(bmi);
+
+                // Display the result in the result label
+                resultLabel.Text = $"Your BMI is: {bmi:F2}\nYour category: {category}";
+            }
+            else
+            {
+                // If the input values are not valid
+                resultLabel.Text = "Please enter valid weight and height values.";
+            }
+
+        }
+
+        // Function to determine BMI category based on BMI value
+        private BMICategory GetBMICategory(double bmi)
+        {
+            if (bmi >= 35)
+                return BMICategory.SevereObesity;
+            else if (bmi >= 30)
+                return BMICategory.Obesity;
+            else if (bmi >= 25)
+                return BMICategory.OverWeight;
+            else if (bmi >= 18.5)
+                return BMICategory.NormalWeight;
+            else
+                return BMICategory.Underweight;
+        }
+    }
+}
